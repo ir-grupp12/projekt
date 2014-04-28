b5ceefdfda881f6db365958a80022c1140ac7a87
glindste <gustaflindstedt@gmail.com>
2014-04-28 23:00:20 +0200

    updated api and added initial summarize function

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #!/bin/env python2.7
 
+import re
 import wikipedia as wiki
 from wikipedia.exceptions import WikipediaException, DisambiguationError
 from flask import Flask, request, render_template
@@ -10,16 +11,23 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
-@app.route("/search")
-def search():
+@app.route("/<searchstring>")
+def search(searchstring):
     error = None
+    wikisum = False
     limit = 3
 
+    # Set the search result limit using the limit parameter
     if request.args.has_key("limit"):
         limit = request.args.get("limit")
 
+    # If wikisum is defined, use the default wikipedia summary
+    if request.args.has_key("wikisum"):
+        wikisum = True
+
     try:
-        searchstring = request.args.get("string")
+        searchstring = searchstring.replace("_", "%20") #request.args.get("string")
+        print searchstring
         titles = wiki.search(searchstring, results = limit);
         # Get the pages with the returned titles
         pages = map(get_page, titles)
@@ -28,15 +36,34 @@ def search():
         error = "Invalid search parameters"
         return render_template("search.html", error=error)
 
-    return render_template("search.html", pages=pages)
+    results = []
+    if not wikisum:
+        results = map(summarize, pages)
+    else:
+        results = [(x.title, x.summary) for x in pages]
 
+    return render_template("search.html", results=results)
+
+#
+# Takes a wikipedia article title and gets
+# the WikipediaPage object for that page.
+#
 def get_page(name):
     try:
         page = wiki.page(title=name.title())
     except DisambiguationError as e:
-        # Only get the first disambiguation option
+        # Only get the first disambiguation option for now
         page = wiki.page(title=e.options[0].title())
     return page
+
+#
+# Takes a WikipediaPage object as parameter and returns
+# a tuple of (title, summary)
+#
+def summarize(page):
+    title = page.title
+    summary = re.split(r'\. ', page.content)[0] + ".";
+    return (title, summary)
 
 if __name__ == "__main__":
     app.run(debug=True)

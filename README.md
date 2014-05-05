074abbaeb21d02fbefd45e14aa77bf0e3b8773b2
Joakim Uddholm <joakim@uddholm.com>
2014-05-05 14:40:03 +0200

    Wordcloud functionality

--- README.md
+++ README.md
@@ -8,4 +8,6 @@ Run server with `python server.py`
 Go to url: `localhost:5000/search?query=<searchstring>`
 
 ####Demo server
-`http://109.74.9.126/`
+http://109.74.9.126/
+
+
--- server.py
+++ server.py
@@ -3,8 +3,9 @@
 import re
 import wikipedia as wiki
 from wikipedia.exceptions import WikipediaException, DisambiguationError
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, json
 import wikifetcher
+import operator
 
 app = Flask(__name__)
 
@@ -39,6 +40,47 @@ def search():
         results = map(summarize, results)   
 
     return render_template("search.html", results=results)
+    
+@app.route("/wordcloud")
+def wordcloud():
+    wikisum = False
+    limit = 20
+
+    # Set the search result limit using the limit parameter
+    if request.args.has_key("limit"):
+        limit = request.args.get("limit")
+
+    # If wikisum is defined, use the default wikipedia summary
+    if request.args.has_key("wikisum"):
+        wikisum = request.args.get("wikisum") == "on"
+
+    query = request.args.get("query")        
+    results = wikifetcher.fetch(query, limit, wikisum)
+    
+    content = "".join([content for title, content in results])
+    tags = make_tags(content)
+    return render_template("wordcloud.html", tags=json.dumps(tags))
+    
+def make_tags(content):
+    
+    tags = dict()
+    for word in content.split(" "):
+        w = word.strip().lower()
+        if w == "":
+            continue
+        if w not in tags:
+            tags[w] = 0
+        tags[w] += 1
+
+    sorted_tags = sorted(tags.iteritems(), key=operator.itemgetter(1), reverse=True)
+    tags = dict()
+    for i in xrange(80):
+        if i >= len(sorted_tags):
+            break            
+        tags[sorted_tags[i][0]] = sorted_tags[i][1]
+        
+    
+    return tags
 
 #
 # Takes a WikipediaPage object as parameter and returns
